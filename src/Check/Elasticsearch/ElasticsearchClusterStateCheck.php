5a61a3db3bfecd24d2984871bcfb82c3690621a2
Vitaliy Zhuk <zhuk2205@gmail.com>
2019-10-23 12:39:18 +0300

    Add License and copyright

--- src/Check/Elasticsearch/ElasticsearchClusterStateCheck.php
+++ src/Check/Elasticsearch/ElasticsearchClusterStateCheck.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the FiveLab Diagnostic package.
+ *
+ * (c) FiveLab <mail@fivelab.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types = 1);
 
 namespace FiveLab\Component\Diagnostic\Check\Elasticsearch;
@@ -22,6 +31,9 @@ class ElasticsearchClusterStateCheck implements CheckInterface
      */
     private $connectionParameters;
 
+    /**
+     * @var Client
+     */
     private $client;
 
     /**
--- tests/Check/Eloquent/DatabaseConnectionCheckTest.php
+++ tests/Check/Eloquent/DatabaseConnectionCheckTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the FiveLab Diagnostic package.
+ *
+ * (c) FiveLab <mail@fivelab.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types = 1);
 
 namespace FiveLab\Component\Diagnostic\Tests\Check\Eloquent;
