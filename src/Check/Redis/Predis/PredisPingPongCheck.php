156dff3e46a33c943a3cf85de6b0e4c903ba95cd
Anasatasiia Sh <anastasiya.sh@mserverone.com>
2025-04-16 10:47:16 +0300

    Add PING/PONG strategy for redis health check

---
+++ src/Check/Redis/Predis/PredisPingPongCheck.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the FiveLab Diagnostic package.
+ *
+ * (c) FiveLab <mail@fivelab.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace FiveLab\Component\Diagnostic\Check\Redis\Predis;
+
+use FiveLab\Component\Diagnostic\Check\CheckInterface;
+use FiveLab\Component\Diagnostic\Result\Failure;
+use FiveLab\Component\Diagnostic\Result\Result;
+use FiveLab\Component\Diagnostic\Result\Success;
+use Predis\Client;
+
+readonly class PredisPingPongCheck implements CheckInterface
+{
+    public function __construct(private string $host, private int $port, private ?string $password = null)
+    {
+    }
+
+    public function check(): Result
+    {
+        if (!\class_exists(Client::class)) {
+            return new Failure('The predis/predis not installed.');
+        }
+
+        $parameters = [
+            'host' => $this->host,
+            'port' => $this->port,
+        ];
+
+        if ($this->password) {
+            $parameters['password'] = $this->password;
+        }
+
+        try {
+            $client = new Client($parameters);
+            $client->ping();
+        } catch (\Throwable $e) {
+            return new Failure(\sprintf(
+                'Cannot connect to Redis: %s.',
+                \rtrim($e->getMessage(), '.')
+            ));
+        }
+
+        return new Success('Success connect to Redis.');
+    }
+
+    public function getExtraParameters(): array
+    {
+        // By security we not return password (because many redis instances work in internal network).
+        return [
+            'host' => $this->host,
+            'port' => $this->port,
+        ];
+    }
+}
---
+++ src/Check/Redis/RedisExt/RedisPingPongCheck.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the FiveLab Diagnostic package.
+ *
+ * (c) FiveLab <mail@fivelab.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace FiveLab\Component\Diagnostic\Check\Redis\RedisExt;
+
+use FiveLab\Component\Diagnostic\Check\CheckInterface;
+use FiveLab\Component\Diagnostic\Result\Failure;
+use FiveLab\Component\Diagnostic\Result\Result;
+use FiveLab\Component\Diagnostic\Result\Success;
+
+readonly class RedisPingPongCheck implements CheckInterface
+{
+    public function __construct(private string $host, private int $port, private ?string $password = null)
+    {
+    }
+
+    public function check(): Result
+    {
+        if (!\class_exists(\Redis::class)) {
+            return new Failure('The ext-redis not installed.');
+        }
+
+        $redis = new \Redis();
+
+        \set_error_handler(static function (int $errno, string $errstr) {
+            throw new \Exception($errstr);
+        });
+
+        try {
+            $this->connect($redis);
+            $redis->ping();
+        } catch (\Throwable $e) {
+            return new Failure(\sprintf(
+                'Cannot connect to Redis: %s.',
+                \rtrim($e->getMessage(), '.')
+            ));
+        } finally {
+            \restore_error_handler();
+        }
+
+        return new Success('Success connect to Redis.');
+    }
+
+    public function getExtraParameters(): array
+    {
+        // By security we not return password (because many redis instances work in internal network).
+        return [
+            'host' => $this->host,
+            'port' => $this->port,
+        ];
+    }
+
+    private function connect(\Redis $redis): void
+    {
+        $redis->connect($this->host, $this->port);
+
+        if ($this->password) {
+            $redis->auth($this->password);
+        }
+    }
+}
